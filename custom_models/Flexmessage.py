dbfa7ed3d27da5d6b2840b7fda6fefac41798838
jny616 <78732106+jny616@users.noreply.github.com>
2021-09-02 01:42:54 +0800

    Add files via upload

---
+++ custom_models/Flexmessage.py
@@ -0,0 +1,158 @@
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import *
+
+def flex_message():
+    contents = {"type": "carousel",
+                "contents": [
+                    {
+                    "type": "bubble",
+                    "size": "nano",
+                    "header": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "日期",
+                            "color": "#ffffff",
+                            "align": "start",
+                            "size": "md",
+                            "gravity": "center"
+                        }
+                        ],
+                        "backgroundColor": "#27ACB2",
+                        "paddingTop": "19px",
+                        "paddingAll": "12px",
+                        "paddingBottom": "16px"
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "contents": [
+                            {
+                                "type": "text",
+                                "text": "身體狀況：",
+                                "color": "#8C8C8C",
+                                "size": "sm",
+                                "wrap": True
+                            }
+                            ],
+                            "flex": 1
+                        }
+                        ],
+                        "spacing": "md",
+                        "paddingAll": "12px"
+                    },
+                    "styles": {
+                        "footer": {
+                        "separator": False
+                        }
+                    }
+                    },
+                    {
+                    "type": "bubble",
+                    "size": "nano",
+                    "header": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "日期：",
+                            "color": "#ffffff",
+                            "align": "start",
+                            "size": "md",
+                            "gravity": "center"
+                        }
+                        ],
+                        "backgroundColor": "#FF6B6E",
+                        "paddingTop": "19px",
+                        "paddingAll": "12px",
+                        "paddingBottom": "16px"
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "contents": [
+                            {
+                                "type": "text",
+                                "text": "身體狀況:",
+                                "color": "#8C8C8C",
+                                "size": "sm",
+                                "wrap": True
+                            }
+                            ],
+                            "flex": 1
+                        }
+                        ],
+                        "spacing": "md",
+                        "paddingAll": "12px"
+                    },
+                    "styles": {
+                        "footer": {
+                        "separator": False
+                        }
+                    }
+                    },
+                    {
+                    "type": "bubble",
+                    "size": "nano",
+                    "header": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "日期：",
+                            "color": "#ffffff",
+                            "align": "start",
+                            "size": "md",
+                            "gravity": "center"
+                        }
+                        ],
+                        "backgroundColor": "#A17DF5",
+                        "paddingTop": "19px",
+                        "paddingAll": "12px",
+                        "paddingBottom": "16px"
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "contents": [
+                            {
+                                "type": "text",
+                                "text": "身體狀況：",
+                                "color": "#8C8C8C",
+                                "size": "sm",
+                                "wrap": True
+                            }
+                            ],
+                            "flex": 1
+                        }
+                        ],
+                        "spacing": "md",
+                        "paddingAll": "12px"
+                    },
+                    "styles": {
+                        "footer": {
+                        "separator": False
+                        }
+                    }
+                    }
+                ]
+                }
+    message = FlexSendMessage(alt_text='身體紀錄',contents=contents)
+    return message          
